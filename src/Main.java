74eb340786d341978db14acd72491e2cd298b345
SurajWalkunde <surajwalkunde14@gmail.com>
2023-01-08 00:06:43 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+   /*     System.out.println("Hello world!");
+        System.out.println("Suraj Walkunde");
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(11);
+        integerList.add(13);
+        integerList.add(45);
+        integerList.add(5);
+        integerList.add(1);
+        integerList.add(114);
+        integerList.add(11);
+        for (Integer v:integerList) {
+            System.out.println(v);
+        }
+        System.out.println(integerList.toString());
+
+        Set<Integer> integerSet=new HashSet<>();
+        integerSet.add(11);
+        integerSet.add(13);
+        integerSet.add(45);
+        integerSet.add(5);
+        integerSet.add(1);
+        integerSet.add(114);
+        integerSet.add(11);
+        for (Integer s:integerSet) {
+            System.out.println(s);
+        }
+        System.out.println(integerSet.toString());
+        */
+      /*  Map<Integer,String> stringMap=new HashMap<>();
+        stringMap.put(11,"Suraj");
+        stringMap.put(13,"Kiran");
+        stringMap.put(14,"Mahesh");
+        stringMap.put(16,"jatin");
+        stringMap.put(96,"jatin");
+        System.out.println(stringMap.toString());
+       Set<Integer> keySet =stringMap.keySet();
+
+        for (Integer key:keySet) {
+            String value=stringMap.get(key);
+            System.out.println("Values of Map:- "+value+" And value is: "+key);
+        }
+*/
+    }
+
+
+
+}
